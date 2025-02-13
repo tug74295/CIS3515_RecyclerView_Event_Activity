6625089393aa5c7b4c18c17aa78dd3702fb1f726
Kostandin Jorgji <tug74295@temple.edu>
2025-02-13 12:12:32 -0500

    Added click event to adjust size

--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -6,16 +6,13 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-// TODO(Step 1: Implement click behavior)
+class NumberDisplayAdapter (private val numbers: Array<Int>, private val callBack : (Int)->Unit) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-// TODO (Step 1a: Accept lambda in constructor)
-class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
-
-    // TODO (Step 1b: Invoke lambda via onClickListener)
     inner class NumberViewHolder (layout: View) : RecyclerView.ViewHolder (layout) {
         // enumerate views inside layout
-        val textView = layout.findViewById<TextView>(R.id.textView)
-
+        val textView = layout.findViewById<TextView>(R.id.textView).apply {
+            setOnClickListener {callBack(numbers[adapterPosition])}
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
